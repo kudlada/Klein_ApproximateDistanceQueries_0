e98fecfcf6cb20c0fafa08624bf7a891fd9d87a9
kudlada <52403035+kudlada@users.noreply.github.com>
2019-07-16 04:54:39 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -8,12 +8,12 @@ namespace Klein_ApproximateDistanceQueries_0
 {
     class Program
     {
-        public static string BORDER_FILE = "../../data/borderCZ.json";
+        public static string BORDER_FILE = "../../data/borderCZ_2.json";
         public static string[] MAP_FILES = new string[] {
                 "../../data/3.osm",
                 "../../data/2.osm",
                 "../../data/1.osm",
-              
+
             };
 
         public static string INTERSECTIONS_FILE = "../../data/intersections.txt";
@@ -21,17 +21,25 @@ namespace Klein_ApproximateDistanceQueries_0
 
         public static void Main(string[] args)
         {
-   //        InputHandler.CreateGraphFromOSMFiles(BORDER_FILE, MAP_FILES, OUTPUT_FILE);
-             PlanarGraph pg = OSMGraphHandler.CreatePlanarGraph
-                 (OUTPUT_FILE, INTERSECTIONS_FILE);
+            Graph g = OSMGraphHandler.CreateOSMGraph(
+                OUTPUT_FILE, INTERSECTIONS_FILE);
+            AlgorithmComparison.Go(g);
 
-                                    //     Graph g = GridGraph.Create(10);
-                                    //     PlanarGraph pg = new PlanarGraph(g);
-            
-             // PlanarSeparator sep = new PlanarSeparator();
-             // Dictionary<int, List<long>> sepLevels = sep.Separate(pg);
-             // Image im = new Image("Separator");
-             // im.GetPlanarGraphBNF( pg.planarNodes.Values.ToList(), pg.planarEdges);
+
+            //    InputHandler.CreateGraphFromOSMFiles(BORDER_FILE, MAP_FILES, OUTPUT_FILE);
+            //    PlanarGraph pg = OSMGraphHandler.CreatePlanarGraph(
+            //         OUTPUT_FILE, INTERSECTIONS_FILE,
+            //         out Graph g);
+            //    BoundarySeparation.GetDecompositionTree(pg,g);
+
+            //    Graph g = GridGraph.Create(20);
+            //    PlanarGraph pg = new PlanarGraph(g);
+            //    List<int> tri = new List<int>();
+            //    List<long> cycle = new List<long>();
+            //    PlanarGraph gr=   Triangulation.GetTriangulation(pg);
+            //    SeparatorCycle c = new SeparatorCycle();
+            //    Dictionary<int, List<long>> sepLevels = sep.Separate(pg,out cy);
+            //    TestingGraphs.PlanarUtilsTest.TestCountOfEdges(30);
         }
     }
 }
